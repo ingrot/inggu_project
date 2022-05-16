dba99aba575adb62480a37650c0a1d4d38e646c5
ingrot <85225816+ingrot@users.noreply.github.com>
2022-05-16 23:58:02 +0300

    Create change_header_to_add_main_body_id.php

---
+++ change_header_to_add_main_body_id.php
@@ -0,0 +1,10 @@
+# /bitrix/templates/inggu/header.php:101
+
+<body 
+<?php
+    $curPage = $APPLICATION->GetCurPage(true);
+    if (
+            $curPage == SITE_DIR."index.php"
+    ){?>
+id="body-id"
+<?}?>>
